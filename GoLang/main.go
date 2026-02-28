c4b03b17133af5f5fca47fa8be6415555235ade5
FlorisKL <302446160@student.rocmondriaan.nl>
2026-02-28 23:35:55 +0100

    update 2 guesses

--- GoLang/main.go
+++ GoLang/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
 
@@ -12,6 +11,13 @@ fmt.Println("Hmmm! Welk nummer denk ik nu aan?")
 
 nummer := rand.Intn(100) + 1
 
+var gok int
 
+	if gok == nummer {
+		fmt.Println("Gefeliciteerd! Je hebt het juiste nummer geraden!")
+	} if gok < nummer {
+		fmt.Println("Helaas, het nummer is hoger. Probeer het opnieuw.")
+
+	}
 
 }
