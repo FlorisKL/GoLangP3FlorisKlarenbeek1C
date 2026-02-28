746c02c05bc911212c7b0704b6e9910ef9782fd0
FlorisKL <302446160@student.rocmondriaan.nl>
2026-02-28 23:55:37 +0100

    update 4 made game function

--- GoLang/main.go
+++ GoLang/main.go
@@ -7,12 +7,15 @@ import (
 
 func main() {
 
-fmt.Println("Hmmm! Welk nummer denk ik nu aan?")
+fmt.Println("Welkom bij het nummer gokspel! Raad het nummer tussen 1 en 100.")
+} 
+func gokspel() bool {
+
+	fmt.Println("Hmmm! Welk nummer denk ik nu aan?")
 
 nummer := rand.Intn(100) + 1
 
-var gok int
-
+	var gok int
 if gok < nummer {
 		fmt.Println("Helaas, het nummer is hoger. Probeer het opnieuw.")
 		return false
@@ -21,9 +24,9 @@ if gok < nummer {
 		fmt.Println("Helaas, het nummer is lager. Probeer het opnieuw.")
 		return false
 
-	} else gok == nummer {
+	} else if gok == nummer {
 		fmt.Println("Gefeliciteerd! Je hebt het juiste nummer geraden!")
 		return true
-		
-}
+	}
+	return false
 }
